c85207fd189ad1ef496bde75a5844609664c1237
hezhengmin <zhengmin099@gmail.com>
2021-07-02 01:19:02 +0800

    modified-繼承應用(父virtual、子override)

---
+++ Application/ViewModels/Error/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.ViewModels
+{
+    public class Error
+    {
+        public string Message { get; set; }
+        public virtual void Show() {
+            Message = "Error";             
+        }
+    }
+}
---
+++ Application/ViewModels/Error/ErrorViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Application.ViewModels
+{
+    public class ErrorViewModel : Error
+    {
+        public ErrorViewModel()
+        {
+            Show();
+        }
+
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public override void Show()
+        {
+            Message = "ErrorViewModel";
+        }
+    }
+}
